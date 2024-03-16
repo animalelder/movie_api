b7b1f026b333267a6dff3644e01849935d34dc3f
animalelder <wllearned@icloud.com>
2024-03-15 18:01:41 -0700

    Added Endpoints & Documentation
    Added the endpoints to index.js with temporary responses.
    
    Added an array of objects called movieList that is returned as JSON object when user visits /movies/
    
    Added to Documentation.html with information on the functions and endpoints of the API
    
    Came up with potential name for API:
    
    cineData

--- index.js
+++ index.js
@@ -10,46 +10,56 @@ const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {
 	flags: 'a',
 });
 
-let topTenMovies = [
+let movieList = [
 	{
-		title: 'Spirited Awau',
+		title: 'Spirited Away',
 		Director: 'Hayao Miyazaki',
+		genre: 'Animation',
 	},
 	{
 		title: 'Almost Famous',
 		Director: 'Cameron Crowe',
+		genre: 'Drama',
 	},
 	{
 		title: 'Everything Everywhere All At Once',
 		Director: 'The Daniels',
+		genre: 'Fantasy',
 	},
 	{
 		title: 'Blade Runner ',
 		Director: 'Ridley Scott',
+		genre: 'Fantasy',
 	},
 	{
 		title: 'A Woman is a Woman',
 		Director: 'Jean-Luc Godard',
+		genre: 'French New Wave',
 	},
 	{
 		title: 'Band of Outsiders',
 		Director: 'Jean-Luc Godard',
+		genre: 'French New Wave',
 	},
 	{
 		title: 'Singin in the Rain',
 		Director: 'Stanley Donen',
+		genre: 'Musical',
 	},
 	{
 		title: 'North By Northwest',
 		Director: 'Alfred Hitchcock',
+		genre: 'Thriller',
 	},
 	{
 		title: 'Paprika',
 		Director: 'Satoshi Kon',
+		genre: 'Anime',
 	},
 	{
 		title: 'Once',
 		Director: 'John Carney',
+		genre: 'Musical',
 	},
 ];
 
@@ -66,7 +76,42 @@ app.get('/', (req, res) => {
 });
 
 app.get('/movies', (req, res) => {
-	res.json(topTenMovies);
+	res.json(movieList);
+});
+
+app.get('/movies/:movieTitle', (req, res) => {
+	res.json('Successful GET request with information on requested movie');
+});
+
+app.get('/movies/genres/:genreName', (req, res) => {
+	res.json('Succesful GET request with list of movies in requested genre');
+});
+
+app.get('/movies/directors/:dirName', (req, res) => {
+	res.json('Succesful GET request with information of requested director');
+});
+
+// POST requests
+app.post('/users', (req, res) => {
+	res.json('Successfully POST request to add new user account');
+});
+
+// PUT requests
+app.put('/users/:userName', (req, res) => {
+	res.json('Successful PUT request to edit user info');
+});
+
+app.put('/users/:userName/favorites/:movieID', (req, res) => {
+	res.json('Successful PUT request to add movie to favorites');
+});
+
+// DELETE requests
+app.delete('/users/:username/favorites/:movieID', (req, res) => {
+	res.json('Successful DELETE request to remove movie from favorites');
+});
+
+app.delete('/users/:userName', (req, res) => {
+	res.json('Successful DELETE request to remove user account');
 });
 
 // error handling
